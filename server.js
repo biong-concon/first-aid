462d89d2970af33f4af4e89c358625048e228856
Biong Concon <appworkxz@gmail.com>
2015-01-23 11:26:39 +0800

    Updated
    Updated

--- server.js
+++ server.js
@@ -1,25 +1,57 @@
+// dependencies
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var WebSocketServer = require('ws').Server
-var http = require('http');
-var server = http.createServer(function(request, response) {
-console.log((new Date()) + ' Received request for ' + request.url);
-response.writeHead(200, {'Content-Type': 'text/plain'});
-response.write("Welcome to Node.js on OpenShift!\n\n");
-response.end("Thanks for visiting us! \n");
+
+// array of users
+var users = new Array();
+
+io.on('connection', function(socket) {
+	console.log('New User Connected...');
+	
+	// disconnected user
+	socket.on('disconnect', function() {
+		console.log('User Disconnected!');
+		
+		// send data about disconnected user
+		io.emit('disconnect', users[socket.id]);
+	});
+	
+	// a new user connected
+	socket.on('usersConnect', function(data) {
+		console.log('Data added to new connected user!');
+	
+		// save data about connected user
+		if (data !== undefined) {
+			users[socket.id] = data;
+		}
+	});
+	
+	// received a help request
+	socket.on('rescueThis', function(data) {
+		console.log('Rescue Data: ' + data);
+		
+		io.emit('rescueThis', data);
+	});
+	
+	// received a rescue respond
+	socket.on('rescueOnWay', function(data) {
+		console.log('Rescue Respond Data: ' + data);
+		
+		io.emit('rescueOnWay', data);
+	});
+	
+	// received active rescuer
+	socket.on('Rescuer', function(data) {
+		console.log('Active rescuer data: ' + data);
+		
+		io.emit('Rescuer', data);
+	});
 });
-server.listen( port, ipaddress, function() {
-console.log((new Date()) + ' Server is listening on port 8080');
+
+http.listen(port, ipaddress, function() {
+	console.log('Listening on: *:' + port);
 });
-wss = new WebSocketServer({
-server: server,
-autoAcceptConnections: false
-});
-wss.on('connection', function(ws) {
-console.log("New connection");
-ws.on('message', function(message) {
-ws.send("Received: " + message);
-});
-ws.send('Welcome!');
-});
-console.log("Listening to " + ipaddress + ":" + port + "...");
